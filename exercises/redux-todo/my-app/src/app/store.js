41537da1b22cce0dee5151c292799ec8d5843dae
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2022-08-11 09:35:13 +0000

    add todo state

--- exercises/redux-todo/my-app/src/app/store.js
+++ exercises/redux-todo/my-app/src/app/store.js
@@ -4,5 +4,6 @@ import counterReducer from '../features/counter/counterSlice';
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    todoChecker: todoCheckReducer,
   },
 });
---
+++ exercises/redux-todo/my-app/src/features/todo/todoSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const todoSlice = createSlice(
+    {
+        name:"todoChecker",
+        initialState: {
+            isChecked: false
+        },
+        reducers:{
+            
+        }
+    }
+)
