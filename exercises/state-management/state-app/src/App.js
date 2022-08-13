3e8617f30e9fb3fc0033861572aea7b716c3ccd3
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2022-08-13 12:33:04 +0000

    init state

--- exercises/state-management/state-app/src/App.js
+++ exercises/state-management/state-app/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useReducer} from 'react'
 function App() {
   return (
     <div className="App">
@@ -12,15 +12,40 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.freecodecamp.org/news/how-to-manage-state-in-your-react-apps/#:~:text=Local%20state%20is%20perhaps%20the,including%20primitive%20and%20object%20values."
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React State
         </a>
       </header>
+      <VoteCounter/>
     </div>
   );
 }
 
+const initialState = { votes: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'upvote':
+      return {votes: state.votes + 1};
+    case 'downvote':
+      return {votes: state.votes - 1};
+    default:
+      throw new Error();
+  }
+}
+function VoteCounter() {
+  const [state] = useReducer(reducer, initialState);
+  return (
+    <>
+    <h1>Value {state.votes}</h1>
+    <button>+</button>
+    <button>-</button>
+    </>
+    
+  )
+  
+}
 export default App;
