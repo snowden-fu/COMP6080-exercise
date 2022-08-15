2c5ebfb9a0e64cce2f0b9839c4b0c69572215b2b
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2022-08-15 20:24:10 +1000

    todo

--- exercises/react-typescript/my-app/src/App.tsx
+++ exercises/react-typescript/my-app/src/App.tsx
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -18,8 +18,51 @@ function App() {
           quick start for react in typescript
         </a>
       </header>
+      <TodoList todoList={todoData}/>
     </div>
   );
 }
-
+interface Todo {
+  id: number;
+  desc: string;
+  isDone: boolean;
+}
+const todoData: Array<Todo> = [
+  { id: 1, desc: "coding", isDone: false },
+  { id: 2, desc: "coding2", isDone: false },
+  { id: 3, desc: "coding3", isDone: false },
+];
+type TodoItemProps = {
+  todo: Todo;
+};
+function TodoItem({ todo }: TodoItemProps) {
+  return (
+    <>
+      <input
+        type={"checkbox"}
+        id={todo.id.toString()}
+        name={todo.id.toString()}
+        defaultChecked={todo.isDone}
+      />
+      <label htmlFor={todo.id.toString()}>{todo.desc}</label>
+      <br></br>
+    </>
+  );
+}
+type TodoListProps = {
+  todoList: Array<Todo>;
+};
+function TodoList({ todoList }: TodoListProps) {
+  return (
+    <>
+      {todoList.map((todo: Todo) => {
+        return (
+          <>
+          <TodoItem todo={todo} key={todo.id}/>
+          </>
+        );
+      })}
+    </>
+  );
+}
 export default App;
